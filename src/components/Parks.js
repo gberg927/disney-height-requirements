63dc0a6f7856e12e46c3eccda070cd89c70818b7
Dennis Glasberg <dennis.glasberg@flywire.com>
2019-11-26 17:09:15 -0500

    Made parks use a table body

--- src/components/Parks.js
+++ src/components/Parks.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import RidesTable from './RidesTable';
 import Park from './Park';
 
-const Parks = ({ parks, riders, isPlaceholderRiders, handleRemoveRider }) =>
-  parks.map(({ node: park }) => (
-    <Park park={park} riders={riders} isPlaceholderRiders={isPlaceholderRiders} handleRemoveRider={handleRemoveRider} />
-  ));
+const Parks = ({ parks, riders, isPlaceholderRiders, handleRemoveRider }) => (
+  <RidesTable riders={riders} isPlaceholderRiders={isPlaceholderRiders} handleRemoveRider={handleRemoveRider}>
+    {parks.map(({ node: park }) => (
+      <Park park={park} riders={riders} isPlaceholderRiders={isPlaceholderRiders} handleRemoveRider={handleRemoveRider} />
+    ))}
+  </RidesTable>
+);
 
 Parks.propTypes = {
   parks: PropTypes.array.isRequired,
--- src/components/RidesTable.js
+++ src/components/RidesTable.js
@@ -2,21 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import RidesTableHeaderRow from './RidesTableHeaderRow';
-import RidesTableRow from './RidesTableRow';
 
-const RidesTable = ({ rides, riders, isPlaceholderRiders, handleRemoveRider }) => (
+const RidesTable = ({ children, riders, isPlaceholderRiders, handleRemoveRider }) => (
   <div className="row">
     <div className="col-md-12">
       <div className="table-responsive">
         <table className="table table-sm table-striped table-hover ">
           <RidesTableHeaderRow riders={riders} isPlaceholderRiders={isPlaceholderRiders} handleRemoveRider={handleRemoveRider} />
-          <tbody>
-            {rides
-              .filter(ride => ride.type === 'Attraction')
-              .map(ride => (
-                <RidesTableRow ride={ride} riders={riders} isPlaceholderRiders={isPlaceholderRiders} />
-              ))}
-          </tbody>
+          {children}
         </table>
       </div>
     </div>
@@ -24,7 +17,7 @@ const RidesTable = ({ rides, riders, isPlaceholderRiders, handleRemoveRider }) =
 );
 
 RidesTable.propTypes = {
-  rides: PropTypes.object.isRequired,
+  children: PropTypes.array.isRequired,
   riders: PropTypes.array.isRequired,
   isPlaceholderRiders: PropTypes.bool.isRequired,
   handleRemoveRider: PropTypes.func.isRequired,
